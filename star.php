ff6d86e3a88ca14933e1ee1303a395df92f5bb4b
thisisyoobi <75841024+thisisyoobi@users.noreply.github.com>
2022-12-28 04:55:38 +0900

    add star service

---
+++ star.php
@@ -0,0 +1,40 @@
+<!--
+::: CONTENTS :::
+Project		: php_website
+Version		: 1.0
+Filename	: view.php
+Date		: 2020/01/08
+Purpose		: Ready for studying secure coding of WEB(PHP)
+Programmer	: Yoobi (ubyung1@gmail.com)
+Reviewer	:
+-->
+
+<?php
+	//Connect MYSQL
+	$connect = mysqli_connect('localhost', 'yoobi', 'toor', 'php_db') or die("connect fail");
+	$number = $_GET['number'];
+	session_start();
+
+	//Check SESSION
+	if(!isset($_SESSION['userid']))
+	{
+?>
+		<script>
+			alert("Login First!");
+			history.back();
+		</script>
+<?php
+		exit;
+	}
+
+	//add hit=hit+1 for view count
+	$hit = "update board set star=star+1 where number=$number";
+	$connect->query($hit);
+	$query = "select title, content, id, date, hit, star, name_orig from board where number =$number";
+	$result = $connect->query($query);
+	$rows = mysqli_fetch_assoc($result);
+
+?>
+<script>
+	history.back();
+</script>
