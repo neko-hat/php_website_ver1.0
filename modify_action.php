01630a83c4eaacaa024b0f56aafa89af5d84ff2e
thisisyoobi <75841024+thisisyoobi@users.noreply.github.com>
2022-12-28 04:55:04 +0900

    update download service

--- modify_action.php
+++ modify_action.php
@@ -23,7 +23,7 @@ Reviewer	:
 		$file = $_FILES['upfile'];
 		$upload_directory='./up_file/';
 		$file_name = $file['name'];
-		$name_save = $number.'_'.$file_name;
+		$name_save = $file_name;
 
 		$max_file_size = 5242880;
 		
