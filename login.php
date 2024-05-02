480901acfb74cfd97c4238751b57dd4ffdc62a44
neko_hat <dohwan90@gmail.com>
2024-05-02 22:24:49 +0900

    patch origin

--- login.php
+++ login.php
@@ -1,53 +1,56 @@
 <!--
 ::: CONTENTS :::
-Project         : php_website
-Version         : 1.0
-Filename        : login.php
-Date            : 2020/01/05
-Purpose         : Ready for studying secure coding of WEB(PHP)
-Programmer      : Yoobi (ubyung1@gmail.com)
-Reviewer        :
+Project		: php_website
+Version		: 1.0
+Filename	: login.php
+Date		: 2020/01/05
+Purpose		: Ready for studying secure coding of WEB(PHP)
+Programmer	: Yoobi (ubyung1@gmail.com)
+Reviewer	:
 -->
 
 <?php
-        session_start();
-?>
+	session_start();
+?> 
 <html>
-        <head>
-                <meta charset='utf-8'>
-        </head>
+	<head>
+		<meta charset='utf-8'>
+	</head>
 
-        <body>
+	<body>
 <?php
-        //If SESSION is not exist
-        if(!isset($_SESSION["userid"]))
-        {
+	//If SESSION is not exist
+	if(!isset($_SESSION["userid"]))	
+	{
 ?>
-                <center>
-                        <form method='get' action='./login_action.php'>
-                                <input name="id" type="text" placeholder="ID" required autofocus><br>
-                                <input name="pw" type="password" placeholder="PASSWORD"><br><br>
-                                <input type="submit" value="SIGN IN"><br>
-                        </form>
-                        <button id="join" onclick="location.href='./join.html'">SIGN UP</button>
-                </center>
+		<center>
+			<form method='get' action='./login_action.php'>
+				<input name="id" type="text" placeholder="ID" required autofocus><br>
+				<input name="pw" type="password" placeholder="PASSWORD"><br><br>
+				<input type="submit" value="SIGN IN"><br>
+			</form>
+			<button id="join" onclick="location.href='./join.html'">SIGN UP</button>
+		</center>
 <?php
-        } else if ($_SESSION['userid'] == "admin") {
+	} else if ($_SESSION['userid'] == "admin") {
 ?>
-                <script>
-                        window.open('./board_list_admin.php', target="_blank");
-                </script>
+		<script>
+			window.open('./board_list_admin.php', target="_blank");
+		</script>
 <?php
-        } else {
-
+	} else {
+		
 ?>
-                <center>
-                        Welcome! <?=$_SESSION["userid"]?><br><br>
-                        <button onclick="location.href='./change_pw.php'">비밀번호 변경</button>
-                        <button onclick="location.href='./logout.php'">SIGN OUT</button>
-                </center>
+		<center>
+			Welcome! <?=$_SESSION["userid"]?><br><br>
+			<button onclick="location.href='./change_pw.php'">비밀번호 변경</button>
+			<button onclick="location.href='./logout.php'">SIGN OUT</button>
+		</center>
 <?php
-        }
+	}
 ?>
-        </body>
+	</body>
 </html>
+
+
+
