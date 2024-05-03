3052e5ef07abcc1e7112f73dcefc817103d3c2ca
neko_hat <dohwan90@gmail.com>
2024-05-03 18:18:04 +0900

    patch write_action_sub

--- write_action_sub.php
+++ write_action_sub.php
@@ -1,4 +1,21 @@
 <?php
+	session_start();
+	//Connect MYSQL
+	$URL = "./board_list.php";
+
+	// If user do not have SESSION back to $URL
+	if(!isset($_SESSION['userid']))
+	{
+?>
+		<script>
+			alert("Login First!");
+			location.replace("<?php echo $URL?>");
+		</script>
+<?php
+	die;
+	}
+?>
+<?php
 	//Connect MYSQL
 	$connect = mysqli_connect("localhost", "yoobi", "toor", "php_db") or die("fail");
 
